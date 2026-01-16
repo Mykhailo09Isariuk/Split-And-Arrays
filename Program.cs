0c34344a7bfccde51787ea6b73d0431033ebb9f5
Mykhailo09Isariuk <isariuk.mykhailo@gmail.com>
2026-01-16 19:59:24 +0200

    Revised Project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Split
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введіть речення >>>  ");
+            string sentence = Console.ReadLine();
+
+            // Розбиваємо речення на слова
+            string[] words = sentence.Split('');
+
+            // Створюємо колекцію
+            List<string> coll = new List<string>(words);
+
+            // Виводимо кожне слово
+            for (int i =0; i < coll.Count; i++)
+            {
+                Console.WriteLine($"Слово {i + 1}: {coll[i]}");
+            }
+
         }
     }
 }
